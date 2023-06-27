16e2eacdfa49c6b74093fed112035c4ed8bb5e5d
intelliDean <intelliDean@gmail.com>
2023-06-27 12:58:25 +0100

    added ifStatement

---
+++ src/main/kotlin/IfStatement.kt
@@ -0,0 +1,15 @@
+fun ifStatement() {
+    val name = "Jordan"
+    val age = 12
+
+    val result =
+        if (name == "Jordan" && age == 12) "This is correct"
+        else "This is incorrect"
+
+
+    println(result)
+}
+
+fun main() {
+    ifStatement()
+}
---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,21 @@
+fun variables() {
+    val name = "Dean"
+    val age = 18
+
+    println("Your name is $name and your age is $age")
+
+    val maxInt = Int.MAX_VALUE
+    val minInt = Int.MIN_VALUE
+
+    println("$maxInt - $minInt")
+
+    val number: Int = 30
+    var string: String = "School"
+    var salary: Double = 230090.90
+    var boolean: Boolean = false
+    var char: Char = 'D'
+}
+
+fun main() {
+    variables()
+}
