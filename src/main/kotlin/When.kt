5899526189fa120652531f44305472863e960cbe
intelliDean <intelliDean@gmail.com>
2023-06-28 12:28:37 +0100

    added few methods

--- src/main/kotlin/When.kt
+++ src/main/kotlin/When.kt
@@ -15,7 +15,47 @@ fun rangeSwitch() {
     }
 }
 
+fun holder() {
+    val num1 = 23
+    val num2 = 43
+
+    println("$num1 + $num2 = ${num1 + num2}")
+}
+
+fun oneHolder() {
+    val age = 23
+    val name = "John"
+
+    println("My name is $name and I am $age years old")
+}
+
+fun doubleHolder() {
+    val number1 = 43
+    val number2 = 89
+    val number3 = 84
+    val number4 = 78
+
+    println(
+        "$number1 + $number2 + $number3 + $number4 = ${number1 + number2 + number3 + number4}"
+    )
+}
+
+fun abbrev() {
+    var num1 = 34
+    var num2 = 56
+
+    num1 += num1 + 32
+    num2 += 23
+
+    println(num1)
+    println(num2)
+}
+
 fun main(args: Array<String>) {
     //switch()
-    rangeSwitch()
+    //rangeSwitch()
+    //holder()
+//    oneHolder()
+    //doubleHolder()
+    abbrev()
 }
